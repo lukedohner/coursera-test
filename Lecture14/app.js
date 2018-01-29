2f2e7572e8c451bda163691dc6f6871a8fc8308c
Luke <lukedohner@gmail.com>
2018-01-29 13:17:34 -0500

    updating lec 14

--- Lecture14/app.js
+++ Lecture14/app.js
@@ -4,38 +4,42 @@
 angular.module('CounterApp', [])
 .controller('CounterController', CounterController);
 
-CounterController.$inject = ['$scope'];
-function CounterController($scope) {
+CounterController.$inject = ['$scope', '$timeout'];
+function CounterController($scope, $timeout) {
   $scope.onceCounter = 0;
   $scope.counter = 0;
   $scope.name = "Yaakov";
 
   $scope.showNumberOfWatchers = function () {
+    console.log($scope);
     console.log("# of Watchers: ", $scope.$$watchersCount);
   };
 
   $scope.countOnce = function () {
     $scope.onceCounter = 1;
   };
-
-  $scope.upCounter = function () {
+$scope.upCounter = function () {
+  $timeout( function(){
     $scope.counter++;
-  };
+     console.log("upCounter fired");
+  }, 2000);
+};
 
-  $scope.$watch(function () {
-    console.log("Digest Loop Fired!");
-  })
+  // $scope.upCounter = function () {
+  //     setTimeout (function (){
+  //     $scope.counter++;
+  //     console.log("upCounter fired");
+  //     $scope.$digest();
+  //     }, 2000);
+  // };
 
+  // $scope.$watch ( function () {
+  //   console.log("Digest Loop Fired!");
+  // });
+}
   // $scope.$watch('onceCounter', function (newValue, oldValue) {
   //   console.log("onceCounter old value: ", oldValue);
   //   console.log("onceCounter new value: ", newValue);
   // });
   //
-  // $scope.$watch('counter', function (newValue, oldValue) {
-  //   console.log("counter old value: ", oldValue);
-  //   console.log("counter new value: ", newValue);
-  // });
-
-}
-
 })();
