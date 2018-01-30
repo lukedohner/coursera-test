7ef6efccd2e3c090c830d02365abd23de660badb
Luke <lukedohner@gmail.com>
2018-01-30 13:03:02 -0500

    adding Lecture20_createdbyLd

--- Lecture19/prototype.js
+++ Lecture19/prototype.js
@@ -5,18 +5,22 @@
 //     objValue: "parentObjValue"
 //   },
 //   walk: function () {
-//     console.log("walking!");
+//     console.log("walk!");
 //   }
 // };
-//
+
 // var child = Object.create(parent);
+
+//child.walk("cool words");
+
+
 // console.log("CHILD - child.value: ", child.value);
 // console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
 // console.log("PARENT - parent.value: ", parent.value);
 // console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 // console.log("parent: ", parent);
 // console.log("child: ", child);
-//
+// //
 // child.value = "childValue";
 // child.obj.objValue = "childObjValue";
 // console.log("*** CHANGED: child.value = 'childValue'");
@@ -27,23 +31,27 @@
 // console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 // console.log("parent: ", parent);
 // console.log("child: ", child);
-//
-// console.log("child.obj === parent.obj ? ", child.obj === parent.obj);
-//
+// //
+//  console.log("child.obj === parent.obj ? ", child.obj === parent.obj);
+// //
 // var grandChild = Object.create(child);
+// grandChild.value = "grandChildValue";
+
 // console.log("Grandchild: ", grandChild);
 // grandChild.walk();
 
 //** Function constructors
 // See my other course: HTML, CSS, and Javascript for Web Developers
 // Lecture #48
-// function Dog(name) {
-//   this.name = name;
-//   console.log("'this' is: ", this);
-// }
-//
-// var myDog = new Dog("Max");
-// console.log("myDog: ", myDog);
-//
-// // Not being used as a function constructor
-// Dog("Max2");
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  console.log("'this' is: ", this);
+}
+
+var myDog = new Dog("Max", "Black");
+console.log("myDog: ", myDog);
+
+// Not being used as a function constructor
+var myDog2 = new Dog("Max2018", "blue");
+console.log("myDog2: ", myDog2);
