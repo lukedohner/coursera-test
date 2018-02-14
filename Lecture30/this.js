706ce9f531fc81b6b70fa366081034030a72ecab
Luke <lukedohner@gmail.com>
2018-02-14 15:17:43 -0500

    adding lec 33 34 35

--- Lecture30/this.js
+++ Lecture30/this.js
@@ -14,3 +14,22 @@ yaakov.describe();
 var describe = yaakov.describe;
 describe();
 describe.call(yaakov);
+
+////////
+function Cat() {
+  this.fullName = "Fox";
+  this.fav = "Laps";
+
+  this.describe = function () {
+    console.log('this is: ', this);
+    console.log(this.fullName + " likes " + this.fav);
+  };
+}
+
+var fox = new Cat();
+fox.describe();
+
+var describe2 = fox.describe;
+describe2();
+
+describe2.call(yaakov);
